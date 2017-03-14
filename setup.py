60d2c90f90b9b7eb47e4f4fb15680dba975d4144
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-03-14 19:43:34 +0100

    Prepare v0.0.16 release

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ class install(_install):
 
 setuptools.setup(
   name = 'node.py',
-  version = '0.0.15',
+  version = '0.0.16',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   license = 'MIT',
