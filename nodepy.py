d050d3a0849477163e5921696986d1e0569b9c19
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-03-28 13:05:02 +0200

    late fix for the __version__ in nodepy.py

--- nodepy.py
+++ nodepy.py
@@ -27,7 +27,7 @@ the same time.
 from __future__ import absolute_import, division, print_function
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '0.0.17'
+__version__ = '0.0.18'
 __license__ = 'MIT'
 
 import argparse
