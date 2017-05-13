2caa9afb2eabcb87117f263ba79a3a61e2b39cee
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-05-13 12:59:53 +0200

    Revert "copy contextlib.nested() from Python 2.7.13 and use it instead of the with statement with multiple objects (see #37) for Python 2.6 compatibility"
    This reverts commit cb25000b3e88fb771c14e2d7f6109dc101da97b0.

--- nodepy.py
+++ nodepy.py
@@ -883,45 +883,6 @@ class Context(object):
         request=request, parent=parent)
 
 
-# From contextlib from Python 2.7.13 with slight modifications.
-@contextlib.contextmanager
-def nested(*managers):
-  """Combine multiple context managers into a single nested context manager.
-  This function has been deprecated in favour of the multiple manager form
-  of the with statement.
-  The one advantage of this function over the multiple manager form of the
-  with statement is that argument unpacking allows it to be
-  used with a variable number of context managers as follows:
-    with nested(*managers):
-        do_something()
-  """
-  exits = []
-  vars = []
-  exc = (None, None, None)
-  try:
-    for mgr in managers:
-      exit = mgr.__exit__
-      enter = mgr.__enter__
-      vars.append(enter())
-      exits.append(exit)
-    yield vars
-  except:
-    exc = sys.exc_info()
-  finally:
-    while exits:
-      exit = exits.pop()
-      try:
-        if exit(*exc):
-          exc = (None, None, None)
-      except:
-        exc = sys.exc_info()
-    if exc != (None, None, None):
-      # Don't rely on sys.exc_info() still containing
-      # the right information. Another exception may
-      # have been raised and caught by an exit method
-      raise six.reraise(*exc)
-
-
 def main(argv=None):
   parser = argparse.ArgumentParser(description=__doc__,
       formatter_class=argparse.RawTextHelpFormatter)
@@ -958,7 +919,7 @@ def main(argv=None):
 
   arguments = args.arguments[:]
   context = Context(args.current_dir, args.verbose)
-  with nested(context, jit_debug(args.debug)):
+  with context, jit_debug(args.debug):
     init = InitModule(context)
     if args.exec_ or not arguments:
       sys.argv = [sys.argv[0]] + arguments
