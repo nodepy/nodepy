f4ae34ff9f6b5518a75492ac56c6ad863823f630
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-09-28 19:17:06 +0200

    add nodepy.utils.compat

---
+++ nodepy/utils/compat.py
@@ -0,0 +1,35 @@
+"""
+Small Python 2/3 compatibility layer.
+"""
+
+import sys
+
+
+if sys.version_info[0] == 3:
+
+  exec_ = exec
+
+  def reraise(tp, value, tb=None):
+    if value is None:
+      value = tp()
+    if value.__traceback__ is not tb:
+      raise value.with_traceback(tb)
+    raise value
+
+
+else:
+
+  def exec_(_code_, _globs_=None, _locs_=None):
+    """Execute code in a namespace."""
+    if _globs_ is None:
+      frame = sys._getframe(1)
+      _globs_ = frame.f_globals
+      if _locs_ is None:
+        _locs_ = frame.f_locals
+      del frame
+    elif _locs_ is None:
+      _locs_ = _globs_
+    exec("""exec _code_ in _globs_, _locs_""")
+
+  exec_("def reraise(tp, value, tb=None):\n"
+        "  raise tp, value, tb")
