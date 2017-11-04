7851515fee09dd7fecbe3fc20af86a5ab2f87e13
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-04 15:44:24 +0100

    update __version__

--- nodepy/__init__.py
+++ nodepy/__init__.py
@@ -3,7 +3,7 @@ The Node.py runtime provides an alternative to standard Python modules
 similar to Node.js.
 """
 
-__version__ = '0.1.0'
+__version__ = '2.0.0-dev'
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
 
 from . import context, runtime
