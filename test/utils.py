bf27f6eb14ea3f1b11a48f7b4b263925c385e01e
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-04 15:16:51 +0100

    skip UrlPath test if no internet connection is present

--- test/utils.py
+++ test/utils.py
@@ -1,11 +1,30 @@
 
-import json
 from nodepy.utils import UrlPath
-from unittest import TestCase, TestSuite
+import json
+import socket
+import unittest
+
+REMOTE_SERVER = "www.google.com"
+
+def is_connected():
+  if hasattr(is_connected, '_result'):
+    return is_connected._result
+  try:
+    # see if we can resolve the host name -- tells us if there is
+    # a DNS listening
+    host = socket.gethostbyname(REMOTE_SERVER)
+    # connect to the host -- tells us if the host is actually
+    # reachable
+    s = socket.create_connection((host, 80), 2)
+    is_connected._result = True
+  except:
+    is_connected._result = False
+  return is_connected._result
 
 
-class TestUrlPath(TestCase):
+class TestUrlPath(unittest.TestCase):
 
+  @unittest.skipIf(not is_connected(), "no internet connection")
   def test_urlpath(self):
     path = UrlPath('https://httpbin.org/get?abc=def')
     with path.open() as fp:
