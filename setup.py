bd3118c7935c8bbd49e67f08b702cd4d9024e38e
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-11-28 15:34:06 +0100

    set version to 2.0.1-dev

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def readme():
 
 setup(
   name = 'nodepy-runtime',
-  version = '2.0.0',
+  version = '2.0.1-dev',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   license = 'MIT',
